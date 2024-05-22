98ee2ade2f6c1a4452be565218f0ed72e09eaace
Jie clark Terec <jieclarkm@gmail.com>
2024-05-23 00:39:47 +0800

    deleting middleware to fix error

--- app/(routes)/mybooking/page.jsx
+++ app/(routes)/mybooking/page.jsx
@@ -5,11 +5,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BookingHistory } from "./_components";
 import GlobalApi from "@/app/_services/GlobalApi";
 import { useSession } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 function MyBooking() {
   const [bookingHistory, setBookingHistory] = useState([]);
   const { session } = useSession();
   const emailAddress = session?.user?.primaryEmailAddress;
+  const { isSignedIn } = useSession();
 
   const completedColor = "bg-[#186a8a0e]";
 
@@ -38,23 +40,29 @@ function MyBooking() {
     return result;
   };
   return (
-    <div className="my-10 mx-5 md:mx-36">
-      <h2 className="my-3 font-bold text-gray-800">Booking History</h2>
-      <Tabs defaultValue="account" className="w-full ">
-        <TabsList className="w-full justify-start  py-5">
-          <TabsTrigger value="booked">Booked Taskers</TabsTrigger>
-          <TabsTrigger value="completed">Completed</TabsTrigger>
-        </TabsList>
-        <TabsContent value="booked">
-          <BookingHistory bookingHistory={filterData("booked")} />
-        </TabsContent>
-        <TabsContent value="completed">
-          <BookingHistory
-            bookingHistory={filterData("completed")}
-            completed={completedColor}
-          />
-        </TabsContent>
-      </Tabs>
+    <div>
+      {isSignedIn ? (
+        <div className="my-10 mx-5 md:mx-36">
+          <h2 className="my-3 font-bold text-gray-800">Booking History</h2>
+          <Tabs defaultValue="account" className="w-full ">
+            <TabsList className="w-full justify-start  py-5">
+              <TabsTrigger value="booked">Booked Taskers</TabsTrigger>
+              <TabsTrigger value="completed">Completed</TabsTrigger>
+            </TabsList>
+            <TabsContent value="booked">
+              <BookingHistory bookingHistory={filterData("booked")} />
+            </TabsContent>
+            <TabsContent value="completed">
+              <BookingHistory
+                bookingHistory={filterData("completed")}
+                completed={completedColor}
+              />
+            </TabsContent>
+          </Tabs>
+        </div>
+      ) : (
+        redirect("https://flying-kangaroo-4.accounts.dev")
+      )}
     </div>
   );
 }
