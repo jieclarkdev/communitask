8fdb669552edf46eedcb21b7666c7b3def3e4338
Jie clark Terec <jieclarkm@gmail.com>
2024-05-22 22:36:16 +0800

    removing middleware clerk

--- middleware.js
+++
@@ -1,7 +0,0 @@
-const { clerkMiddleware } = require("@clerk/nextjs/server");
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
